861e6284ed4cfff369900c3ea223b1c09f3d63fa
Federico_Calandra <federico12597@gmail.com>
2024-09-11 17:10:49 +0200

    Added run_simulation.m

---
+++ run_simulations.m
@@ -0,0 +1,7 @@
+run("simulation_constantVolatility.mlx");
+run("simulation_increasingVolatility_original.mlx");
+run("simulation_increasingVolatility_improvedVLP.mlx");
+run("simulation_increasingVolatility_reserve.mlx");
+run("simulation_increasingVolatility_improvedVLPAndReserves.mlx");
+
+fprintf("Process completed.");
